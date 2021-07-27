d4066dac402a89b3fdd5dbcc06831fc375b185c6
Aran Ailbhe <mosp08@protonmail.com>
2021-07-28 00:57:15 +0300

    Cleaned up and fix numerous mistakes in naming and func calling.

--- mos/bootstrap.py
+++ mos/bootstrap.py
@@ -10,14 +10,14 @@ import sys
 import string
 import tarfile
 
-target_bootstrap_dir = mos.mos_path + "/data/build/bootstrap/" + mos.target_distro
-target_bootstrap_base_dir = mos.mos_path + target_bootstrap_dir + "/base"
+target_bootstrap_dir = mos.mos_path + "/data/build/bootstrap"
+target_bootstrap_build_dir = mos.mos_path + target_bootstrap_dir + "/" + mos.target_distro + "/" + mos.target_id
 
 alpine_mirror = "http://dl-cdn.alpinelinux.org/alpine/"
-alpine_mirror_releases_url = alpine_mirror + "latest-stable/releases/" + arch + "/"
-alpine_mirror_release = alpine_mirror + "latest-stable/releases/" + arch + "/latest-releases.yaml"
+alpine_mirror_releases_url = alpine_mirror + "latest-stable/releases/" + arch
+alpine_mirror_release = alpine_mirror + "/latest-stable/releases/" + arch + "/latest-releases.yaml"
 
-rootfs_targz = "rootfs" + target_distro + arch
+rootfs_targz = "rootfs" + "_" + target_distro + "_" + arch
 
 def target_bootstrap():
 
@@ -32,16 +32,18 @@ def target_bootstrap():
 
 	os.chdir(target_bootstrap_dir)
 
-	latest_release = requests.get(alpine_mirror_release, allow_redirects=True)
+	alpine_latest_release = requests.get(alpine_mirror_release, allow_redirects=True)
 	open("latest-releases.yaml", 'wb').write(alpine_latest_release.content)
 
 	with open("latest-releases.yaml", "r") as file:
 		for line in file:
 			if re.search("file: alpine-minirootfs-.*.tar.gz", line):
 				target_rootfs_id_full = str(line)
-				target_rootfs_id_split = rootfs_id_full.split()
-				target_rootfs_id = rootfs_id_split[1]
+				target_rootfs_id_split = target_rootfs_id_full.split()
+				target_rootfs_id = target_rootfs_id_split[1]
+				print(target_rootfs_id)
 
-	target_rootfs_url = alpine_mirror_releases_dir + rootfs_id
-	rootfs_file = requests.get(target_rootfs_ink, allow_redirects=True)
-	open(rootfs_targz, 'wb').write(target_rootfs.content)
+	target_rootfs_url = alpine_mirror_releases_url + "/" + target_rootfs_id
+	print(target_rootfs_url)
+	target_rootfs_url_request = requests.get(target_rootfs_url, allow_redirects=True)
+	open(rootfs_targz, 'wb').write(target_rootfs_url_request.content)
