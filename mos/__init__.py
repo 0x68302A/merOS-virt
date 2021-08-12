ff0d91db9034bb4adc7f87b65ab04e3f136e1964
Aran Ailbhe <mosp08@protonmail.com>
2021-08-12 10:25:29 +0300

    Cleared imports.

--- mos/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-from mos.helper import *
-from mos.host_conf import *
-from mos.kernel_build import *
-from mos.bootstrap import *
-from mos.chroot_manage import *
-from mos.libvirt_manage import *
