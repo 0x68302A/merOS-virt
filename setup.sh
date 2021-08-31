3b0f2dc8c354cfc54b182eb12a8f2e7041becbfb
Aran Ailbhe <mosp08@protonmail.com>
2021-09-01 01:24:31 +0300

    Added virt-viewer to dependencies, needed to access the GUI part of our targets, via the VNC server hosted by libvirt-qemu.

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ apt install -y \
 	python3 python3-pip \
 	python3-guestfs python3-libvirt \
 	python3-apt rustc libssl-dev \
-	libvirt-daemon-system qemu-kvm libguestfs-tools
+	libvirt-daemon-system qemu-kvm libguestfs-tools virt-viewer
 
 ## We can then install the pip requirments
 pip3 install \
