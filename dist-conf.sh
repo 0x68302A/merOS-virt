275cf949ff234416cfb76b3c8eeca17047448526
Aran Ailbhe <mosp08@protonmail.com>
2022-02-15 21:31:16 +0200

    Rename distro-specifig shell script- It fits better it's purpose.

--- dist-conf.sh
+++ dist-conf.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-## APT Configure
 ## For debian, we need
-
 apt install -y \
 	rsync flex bison bc libelf-dev \
 	python3 python3-pip \
 	python3-guestfs python3-libvirt \
 	python3-apt rustc libssl-dev \
 	libvirt-daemon-system qemu-kvm libguestfs-tools virt-viewer xpra
+
+## TODO:
+## Check for host distro, and configure as expected.
