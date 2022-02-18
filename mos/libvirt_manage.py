abd01dc34a679d41a105f89d50a08a2f548b4b24
Aran Ailbhe <mosp08@protonmail.com>
2022-02-18 17:44:02 +0200

    Continuation on f11fb88- And some formatting improvements.

--- mos/libvirt_manage.py
+++ mos/libvirt_manage.py
@@ -12,6 +12,7 @@ import logging
 
 class LibvirtManage:
 	def __init__(self, target_fam_id):
+
 		self.target_fam_id = target_fam_id
 
 		## Import Global Variables
@@ -21,33 +22,23 @@ class LibvirtManage:
 		self.arch = h.arch
 
 		self.mos_img_dir = self.h.mos_img_dir
-		self.target_rootfs_img = self.mos_img_dir + self.target_fam_id + ".img"
 		self.mos_ssh_priv_key_dir = self.h.mos_ssh_priv_key_dir
 		self.kernel_img = self.mos_img_dir + "bzImage"
 
-		## Identify if we provided
-		## Full Target ID
-		## Or just Family
-		## TODO: Manage Identification through Helper()
-		try:
-			self.target_id_split = self.target_fam_id.split("-")
-			self.target_fam = self.target_id_split[0]
-			self.target_id = self.target_id_split[1]
-		except:
-			self.target_fam = self.target_fam_id
-
-		self.conf_dir = self.mos_path + "/conf/target/" + self.target_fam
+		self.conf_dir = self.mos_path + "/conf/target/" + self.target_fam_id
 		self.xml_dir = self.conf_dir + "/libvirt/"
 		self.hooks_dir = self.conf_dir + "/hooks/"
 
 		## Connect to Hypervisor
 		try:
 			self.conn = libvirt.open("qemu:///system")
+
 		except libvirt.libvirtError:
 			logging.error('Failed to open connection to the hypervisor')
 			sys.exit(1)
 
 	def doms_init(self):
+
 		self.doms = glob.glob(self.xml_dir + "dom_*")
 		for i in self.doms:
 			try:
@@ -63,6 +54,7 @@ class LibvirtManage:
 				xml_data = xml_data.replace('mos_img_dir', self.h.mos_img_dir)
 
 				dom0 = self.conn.createXML(xml_data)
+
 			except libvirt.libvirtError:
 				logging.error('Domain is running, or Failed to Parse XML')
 
@@ -73,11 +65,13 @@ class LibvirtManage:
 	def nets_init(self):
 		self.nets = glob.glob(self.xml_dir + "net_*")
 		for i in self.nets:
+
 			try:
 				self.xml_parse = helper.ParseXML(i)
 				self.xml = self.xml_parse.read_xml()
 				dom0 = self.conn.networkCreateXML(self.xml)
 				logging.info('Created Network interface from %s', i)
+
 			except libvirt.libvirtError:
 				logging.error('Network is running, Target Image is missing, or Failed to Parse XML')
 
@@ -90,6 +84,7 @@ class LibvirtManage:
 			try:
 				subprocess.call(i)
 				logging.info('Applied netfilter rules from %s', i)
+
 			except CalledProcessError:
 				logging.error(CalledProcessError)
 
@@ -97,15 +92,19 @@ class LibvirtManage:
 ## TODO: Create a method to allow for Target-Specific Termination
 class LibvirtTerminate:
 	def __init__(self):
+
 		self.conn = libvirt.open("qemu:///system")
+
 		for i in self.conn.listDomainsID():
 				self.dom = self.conn.lookupByID(i)
 				self.dom.destroy()
 				time.sleep(1)
+
 		for i in self.conn.listNetworks():
 				self.net = self.conn.networkLookupByName(i)
 				self.net.destroy()
 				time.sleep(1)
+
 		if self.conn.listDomainsID():
 			logging.error('ERROR! There are live domains.')
 		else:
