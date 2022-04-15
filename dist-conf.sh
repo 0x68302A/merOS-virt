fb1401a89b0ffa62e524117da88d911194450352
Aaran Ailbhe <mosp08@protonmail.com>
2022-04-15 21:53:01 +0300

    Add net-tools to dependancies.

--- dist-conf.sh
+++ dist-conf.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-## For debian, we need
+## For debian, we depend on:
 apt install -y \
 	rsync flex bison bc libelf-dev \
 	python3 python3-pip \
 	python3-guestfs python3-libvirt \
-	python3-apt rustc libssl-dev \
+	rustc libssl-dev net-tools\
 	libvirt-daemon-system qemu-kvm libguestfs-tools virt-viewer xpra
 
 ## TODO:
 ## Check for host distro, and configure as expected.
+
+## Temp Debian bug fix
+## https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=623536
+mkdir /var/lib/libvirt/dnsmasq/
