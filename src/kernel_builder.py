ff83f77fa39b79b426954b910f1e775dbe9a561f
0x68302A <mosp08@protonmail.com>
2025-04-26 01:11:40 +0300

    fixed --kernel-build, retabed

---
+++ src/kernel_builder.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+
+from src.app_config import AppConfig
+
+import os
+import shutil
+import git
+from git import Repo
+from git import RemoteProgress
+import subprocess
+import logging
+
+class CloneProgress(RemoteProgress):
+    def update(self, op_code, cur_count, max_count=None, message=''):
+        if message:
+            print(message)
+
+## We get the kernel from Git
+## And build, using  custom configutarion
+class KernelBuilder:
+    def __init__(self, verbose: bool = False):
+
+        ## Universal Paths import
+        ## From Config
+        self.mos_path = AppConfig.mos_path
+        self.mos_img_dir = AppConfig.mos_img_dir
+        self.arch = "x86_64"
+        logging.info('System Architecture is %s', self.arch)
+
+        ## Kernel Git URL
+        ## And build path
+        self.kernel_git_url = "https://github.com/torvalds/linux"
+        self.mos_kernel_build_dir = self.mos_path + "/data/build/kernel"
+        self.mos_kernel_git_dir = self.mos_path + "/data/build/kernel/linux"
+
+        ## bzimage build path
+        self.bzimage = self.mos_kernel_git_dir + "/arch/" + self.arch + "/boot/bzImage"
+        ## bzimage Target path
+        self.target_bzimage = self.mos_img_dir + '/bzImage'
+
+    ## Kernel Cloning
+    def kernel_clone(self):
+
+        if os.path.exists(self.mos_kernel_git_dir):
+            pass
+            logging.info('Kernel is already cloned')
+        else:
+            print("cloning into %s" % self.mos_kernel_git_dir)
+            git.Repo.clone_from(self.kernel_git_url, self.mos_kernel_git_dir,
+                depth=1, branch='master', progress=CloneProgress())
+
+
+    ## Kernel Building
+    def kernel_build(self):
+
+        if os.path.isfile(self.target_bzimage):
+            pass
+            logging.info('Kernel is already built')
+        else:
+
+            ## To manage build more efficiently
+            ## we chdir to build Path
+            os.chdir(self.mos_kernel_git_dir)
+
+            ## make prepare calls
+            subprocess.run(['make clean'], shell=True)
+            subprocess.run(['make mrproper'], shell=True)
+            subprocess.run(['make defconfig'], shell=True)
+
+            ## Custom kernel options for Virtualization support
+            self.kernelopts = ("CONFIG_TUN=y"
+                    + "\nCONFIG_VIRTIO_PCI=y"
+                    + "\nCONFIG_VIRTIO_BLK=y"
+                    + "\nCONFIG_OVERLAY_FS=y"
+                    + "\nCONFIG_NET=y"
+                    + "\nCONFIG_NETFILTER=y"
+                    + "\nCONFIG_NETFILTER_ADVANCED=y"
+                    + "\nCONFIG_NF_TABLES=y"
+                    + "\nCONFIG_BRIDGE_NETFILTER=y"
+                    + "\nCONFIG_BRIDGE=y"
+                    + "\nCONFIG_NETFILTER_XT_MATCH_ADDRTYPE=y"
+                    + "\nCONFIG_NETFILTER_XT_TARGET_MASQUERADE=y"
+                    + "\nCONFIG_IP_NF_TARGET_MASQUERADE=y"
+                    + "\nCONFIG_IP_NF_NAT=y"
+                    + "\nCONFIG_VIRTIO_MMIO=y")
+
+            with open('.mos_config', 'w') as f:
+                f.write(self.kernelopts)
+
+            subprocess.run(['./scripts/kconfig/merge_config.sh .config .mos_config'], shell=True)
+
+            ## Final make calls
+            subprocess.run(['make -j $(cat /proc/cpuinfo | grep processor | wc -l)'], shell=True)
+            subprocess.run(['make headers_install'], shell=True)
+            logging.info('Built Linux kernel from source')
+
+            ## Transfer bzimage from Build Path to Target Path - data/images/
+            shutil.copyfile(self.bzimage, self.target_bzimage)
