74ae9469425793fd3f07a205c8eeeb3dd74ff0fd
0x68302A <mosp08@protonmail.com>
2025-04-27 11:24:34 +0300

    retab, remove unused blocks

--- src/ssh_communication.py
+++ src/ssh_communication.py
@@ -22,215 +22,151 @@ import subprocess
 import logging
 
 class SSHCommunication:
-	def __init__(self, target_full_id):
-		self.target_full_id = target_full_id
+    def __init__(self, target_full_id):
+        self.target_full_id = target_full_id
 
-		## Import Gloval Variables
-		h = config.Config()
-		self.h = h
-		self.mos_path  =  h.mos_path
+        ## Import Gloval Variables
+        h = config.Config()
+        self.h = h
+        self.mos_path  =  h.mos_path
 
-		self.target_id_split = self.target_full_id.split("-")
-		self.target_fam = self.target_id_split[0]
-		self.target_id = self.target_id_split[1]
+        self.target_id_split = self.target_full_id.split("-")
+        self.target_fam = self.target_id_split[0]
+        self.target_id = self.target_id_split[1]
 
-		self.target_id_xml = ( self.mos_path + "/conf/"
-			+ self.target_fam + "/build/"
-			+ self.target_id + ".xml" )
+        self.target_id_xml = ( self.mos_path + "/conf/"
+            + self.target_fam + "/build/"
+            + self.target_id + ".xml" )
 
-		self.mos_ssh_priv_key_dir = self.h.mos_ssh_priv_key_dir
-		self.mos_ssh_key = self.mos_ssh_priv_key_dir + "/" + self.target_full_id + "-id_rsa"
+        self.mos_ssh_priv_key_dir = self.h.mos_ssh_priv_key_dir
+        self.mos_ssh_key = self.mos_ssh_priv_key_dir + "/" + self.target_full_id + "-id_rsa"
 
-		## Parse XML
-		## Grabing Target- Specific SSH related Options
-		self.xml_parse = config.ParseXML(self.target_id_xml)
-		self.target_ip = str(self.xml_parse.read_xml_value("network", "ip_addr"))
-		self.target_ssh_port = str(self.xml_parse.read_xml_value("network", "ssh_port"))
-		self.target_username = str(self.xml_parse.read_xml_value("details", "username"))
+        self.k = paramiko.RSAKey.from_private_key_file(self.mos_ssh_key)
 
-		self.k = paramiko.RSAKey.from_private_key_file(self.mos_ssh_key)
-		# self.sshClient = paramiko.SSHClient()
-		# self.sshClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-		# self.sshClient.connect( hostname = self.target_ip, username = self.target_username, port = self.target_ssh_port, pkey = self.k )
+        self.transport = paramiko.Transport((self.target_ip, int(self.target_ssh_port)))
+        self.transport.connect(username = self.target_username, pkey = self.k )
+        self.sftp = paramiko.SFTPClient.from_transport(self.transport)
 
-		# self.channel = self.sshClient.get_transport().open_session()
-		# self.channel.get_pty(term="xterm")
-		# self.channel.invoke_shell()
+    def interactive_shell_native(self):
 
-		self.transport = paramiko.Transport((self.target_ip, int(self.target_ssh_port)))
-		self.transport.connect(username = self.target_username, pkey = self.k )
-		self.sftp = paramiko.SFTPClient.from_transport(self.transport)
+        ssh_command = 'ssh -i ' + self.mos_ssh_key + ' ' + self.target_username + '@' + self.target_ip + ' -p ' + self.target_ssh_port
+        subprocess.run(ssh_command, shell=True)
 
-	def interactive_shell_native(self):
+    def download_files(self, sftp_client, remote_dir, local_dir):
 
-		ssh_command = 'ssh -i ' + self.mos_ssh_key + ' ' + self.target_username + '@' + self.target_ip + ' -p ' + self.target_ssh_port
-		subprocess.run(ssh_command, shell=True)
+        if not self.exists_remote(sftp_client, remote_dir):
+            return
 
+        if not os.path.exists(local_dir):
+            os.mkdir(local_dir)
 
-	## Grabed from Paramiko Examples
-	## With an addition for Flexibe window size handling
-	# thanks to Mike Looijmans for this code
-	def interactive_shell_paramiko(self):
-		chan = self.channel
-		oldtty = termios.tcgetattr(sys.stdin)
-		try:
-			tty.setraw(sys.stdin.fileno())
-			tty.setcbreak(sys.stdin.fileno())
-			chan.settimeout(0.0)
-			signal.signal(signal.SIGWINCH, self.signal_winsize_handler)
-
-			while True:
-				try:
-					r, w, e = select.select([chan, sys.stdin], [], [])
-				except select.error as e:
-					if e[0] != errno.EINTR: raise
-				if chan in r:
-					try:
-						x = u(chan.recv(1024))
-						if len(x) == 0:
-							sys.stdout.write("\r\n*** EOF\r\n")
-							break
-						sys.stdout.write(x)
-						sys.stdout.flush()
-					except socket.timeout:
-						pass
-				if sys.stdin in r:
-					x = sys.stdin.read(1)
-					if len(x) == 0:
-						break
-					chan.send(x)
-
-		finally:
-			termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
-
-		logging.info('connected to Target %s', self.target_full_id)
-
-	def signal_winsize_handler(self, signum, frame):
-		if signum == signal.SIGWINCH:
-			s = struct.pack('HHHH', 0, 0, 0, 0)
-			t = fcntl.ioctl(sys.stdout.fileno(), termios.TIOCGWINSZ, s)
-			winsize = struct.unpack('hhhh', t)
-			self.channel.resize_pty(width=winsize[1], height=winsize[0])
+        for filename in sftp_client.listdir(remote_dir):
+            if stat.S_ISDIR(sftp_client.stat(remote_dir + filename).st_mode):
+                # uses '/' path delimiter for remote server
+                self.download_files(sftp_client, remote_dir
+                                    + filename
+                                    + '/', os.path.join(local_dir, filename))
+            else:
+                if not os.path.isfile(os.path.join(local_dir, filename)):
+                    sftp_client.get(remote_dir + filename, os.path.join(local_dir, filename))
 
 
-	def download_files(self, sftp_client, remote_dir, local_dir):
+    def exists_remote(self, sftp_client, path):
 
-		if not self.exists_remote(sftp_client, remote_dir):
-			return
-
-		if not os.path.exists(local_dir):
-			os.mkdir(local_dir)
-
-		for filename in sftp_client.listdir(remote_dir):
-			if stat.S_ISDIR(sftp_client.stat(remote_dir + filename).st_mode):
-				# uses '/' path delimiter for remote server
-				self.download_files(sftp_client, remote_dir
-									+ filename
-									+ '/', os.path.join(local_dir, filename))
-			else:
-				if not os.path.isfile(os.path.join(local_dir, filename)):
-					sftp_client.get(remote_dir + filename, os.path.join(local_dir, filename))
+        try:
+            sftp_client.stat(path)
+        except IOError as e:
+            if e.errno == errno.ENOENT:
+                return False
+            raise
+        else:
+            return True
 
 
-	def exists_remote(self, sftp_client, path):
+    def target_push(self, file):
+        local_file = os.path.abspath(file)
+        remote_path = "/home/user/mos-shared"
+        remote_file  = os.path.join(remote_path, file)
 
-		try:
-			sftp_client.stat(path)
-		except IOError as e:
-			if e.errno == errno.ENOENT:
-				return False
-			raise
-		else:
-			return True
-
-
-	def target_push(self, file):
-		local_file = os.path.abspath(file)
-		remote_path = "/home/user/mos-shared"
-		remote_file  = os.path.join(remote_path, file)
-
-		if os.path.isdir(local_file):
-			sftp = SFTPClient_push.from_transport(self.transport)
-			sftp.mkdir(remote_path, ignore_existing=True)
-			sftp.put_dir(local_file, remote_path)
-			sftp.close()
+        if os.path.isdir(local_file):
+            sftp = SFTPClient_push.from_transport(self.transport)
+            sftp.mkdir(remote_path, ignore_existing=True)
+            sftp.put_dir(local_file, remote_path)
+            sftp.close()
 
 
-		elif os.path.isfile(local_file):
-			self.sftp.put(local_file, remote_file)
+        elif os.path.isfile(local_file):
+            self.sftp.put(local_file, remote_file)
+
+        logging.info('Transfered "%s" to "%s"', file, self.target_full_id)
+
+    def target_pull(self):
+
+        local_path = os.path.join(self.mos_path, "data/mos-shared/", self.target_full_id)
+        remote_path = "/home/user/mos-shared/"
+        self.download_files(self.sftp, remote_path, local_path)
+
+        logging.info('Pulled data from "%s"', self.target_full_id)
+
+    def target_run(self, run_args):
+
+        shell_waypipe_clean_local = ['rm', '-rf', '/tmp/socket_local']
+
+        shell_waypipe_clean_remote = [
+            'ssh',
+            '-i', self.mos_ssh_key,
+            f"{self.target_username}@{self.target_ip}",
+            '-p', self.target_ssh_port,
+            'rm -rf /tmp/socket_remote'
+        ]
+
+        shell_waypipe_listen = ['/usr/bin/waypipe', '-s', '/tmp/socket_local', 'client']
+
+        shell_waypipe_bind = [
+            'ssh',
+            '-R', '/tmp/socket_remote:/tmp/socket_local',
+            '-i', self.mos_ssh_key,
+            f"{self.target_username}@{self.target_ip}",
+            '-p', self.target_ssh_port,
+            '/usr/bin/waypipe', '-s', '/tmp/socket_remote', 'server',
+            '--',
+            run_args
+        ]
+
+        process_cleanup_local = subprocess.Popen(shell_waypipe_clean_local, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process_cleanup_remote = subprocess.Popen(shell_waypipe_clean_remote, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process_waypipe_listen = subprocess.Popen(shell_waypipe_listen, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        time.sleep(2)
+        process_waypipe_bind = subprocess.Popen(shell_waypipe_bind, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(f"Started Local Waypipe with PID: {process_waypipe_listen.pid}")
+
+        # Debug Waypipe
+        ## with open('output.log', 'w') as outfile:
+        ##     # Start the command and redirect stdout and stderr to the file
+        ##     # process = subprocess.Popen(shell_waypipe_bind, stdout=outfile, stderr=subprocess.STDOUT)
+        ##     process_waypipe_bind = subprocess.Popen(shell_waypipe_bind, stdout=outfile, stderr=subprocess.STDOUT)
 
 
 
+        ##     # Optionally, wait for the process to complete
+        ##     process_waypipe_bind.wait()
 
-		logging.info('Transfered "%s" to "%s"', file, self.target_full_id)
-
-	def target_pull(self):
-
-		local_path = os.path.join(self.mos_path, "data/mos-shared/", self.target_full_id)
-		remote_path = "/home/user/mos-shared/"
-		self.download_files(self.sftp, remote_path, local_path)
-
-		logging.info('Pulled data from "%s"', self.target_full_id)
-
-	def target_run(self, run_args):
-
-		shell_waypipe_clean_local = ['rm', '-rf', '/tmp/socket_local']
-
-		shell_waypipe_clean_remote = [
-			'ssh',
-			'-i', self.mos_ssh_key,
-			f"{self.target_username}@{self.target_ip}",
-			'-p', self.target_ssh_port,
-			'rm -rf /tmp/socket_remote'
-		]
-
-		shell_waypipe_listen = ['/usr/bin/waypipe', '-s', '/tmp/socket_local', 'client']
-
-		shell_waypipe_bind = [
-			'ssh',
-			'-R', '/tmp/socket_remote:/tmp/socket_local',
-			'-i', self.mos_ssh_key,
-			f"{self.target_username}@{self.target_ip}",
-			'-p', self.target_ssh_port,
-			'/usr/bin/waypipe', '-s', '/tmp/socket_remote', 'server',
-			'--',
-			run_args
-		]
-
-		process_cleanup_local = subprocess.Popen(shell_waypipe_clean_local, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		process_cleanup_remote = subprocess.Popen(shell_waypipe_clean_remote, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		process_waypipe_listen = subprocess.Popen(shell_waypipe_listen, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		time.sleep(2)
-		process_waypipe_bind = subprocess.Popen(shell_waypipe_bind, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		print(f"Started Local Waypipe with PID: {process_waypipe_listen.pid}")
-
-		# Debug Waypipe
-		## with open('output.log', 'w') as outfile:
-		##	   # Start the command and redirect stdout and stderr to the file
-		##	   # process = subprocess.Popen(shell_waypipe_bind, stdout=outfile, stderr=subprocess.STDOUT)
-		##	   process_waypipe_bind = subprocess.Popen(shell_waypipe_bind, stdout=outfile, stderr=subprocess.STDOUT)
-
-
-
-		##	   # Optionally, wait for the process to complete
-		##	   process_waypipe_bind.wait()
-
-		##	   print("Process completed. Output written to output.log.")
+        ##     print("Process completed. Output written to output.log.")
 
 class SFTPClient_push(paramiko.SFTPClient):
-	def put_dir(self, source, target):
-		for item in os.listdir(source):
-			if os.path.isfile(os.path.join(source, item)):
-				self.put(os.path.join(source, item), '%s/%s' % (target, item))
-			else:
-				self.mkdir('%s/%s' % (target, item), ignore_existing=True)
-				self.put_dir(os.path.join(source, item), '%s/%s' % (target, item))
+    def put_dir(self, source, target):
+        for item in os.listdir(source):
+            if os.path.isfile(os.path.join(source, item)):
+                self.put(os.path.join(source, item), '%s/%s' % (target, item))
+            else:
+                self.mkdir('%s/%s' % (target, item), ignore_existing=True)
+                self.put_dir(os.path.join(source, item), '%s/%s' % (target, item))
 
-	def mkdir(self, path, mode=511, ignore_existing=False):
-		try:
-			super(SFTPClient_push, self).mkdir(path, mode)
-		except IOError:
-			if ignore_existing:
-				pass
-			else:
-				raise
+    def mkdir(self, path, mode=511, ignore_existing=False):
+        try:
+            super(SFTPClient_push, self).mkdir(path, mode)
+        except IOError:
+            if ignore_existing:
+                pass
+            else:
+                raise
